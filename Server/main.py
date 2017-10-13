f072f41a83d5090e5b03ab3ffe7d24ab657a71f5
Unknown <bambookchos@gmail.com>
2017-10-14 02:47:57 +0300

    One more test

--- Server/main.py
+++ Server/main.py
@@ -47,7 +47,7 @@ def application(environ, start_response):
     <div class="block">
     <img src="title.png" height="300px">
     <h3>Температура: {0} C°  , Влажнсть : {1}%<h3>
-    <h3>Время замера {2}:{3}<h3>
+    <h3>Время замера {2}<h3>
     </div>
     </body>
     </html>

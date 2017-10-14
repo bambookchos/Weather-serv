0ba2cf7bd9089cf0440996139bba00f2393bf9b9
Unknown <bambookchos@gmail.com>
2017-10-14 05:59:10 +0300

    Added ploter

---
+++ Server/ploter.py
@@ -0,0 +1,52 @@
+# coding: utf8
+import log
+import db_work
+from config import cfg
+
+
+
+import random
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import math
+import datetime
+import time
+import sys
+
+
+while True:
+  x = []
+  y = []
+  z = []
+  time = datetime.datetime.now()
+  big_data = db_work.get_data(time - datetime.timedelta(hours=3), time)
+  for i in big_data:
+          x.append(i[0])
+          y.append(i[1])
+          z.append(i[2])
+
+      # plot
+
+
+
+  fig = plt.figure(figsize=(10,10))
+  ax = fig.add_subplot(111)
+  g = [time-datetime.timedelta(hours=3)+datetime.timedelta(minutes=15*i) for i in range(13)]
+  plt.xticks(g,[i.strftime("%H:%M")for i in g])
+
+  ax2 = ax.twinx()
+  ax2.plot(x,z, label="Humidity", color='r')
+  ax.plot(x,y, label="Temperature", color='b')
+  ax.legend(loc='upper left')
+  ax2.legend(loc='upper right')
+
+  ax.grid(True)
+  fig.autofmt_xdate()
+      # beautify the x-labels
+
+
+
+  plt.savefig("../static/3h.png")
+
+  time.sleep(3)

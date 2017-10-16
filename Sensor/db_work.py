c4e8221a8c96ecaf821e36b4b2ab41447a166b9c
Gleb <bambookchos@gmail.com>
2017-10-17 00:40:54 +0300

    Changed DB work

--- Sensor/db_work.py
+++ Sensor/db_work.py
@@ -7,11 +7,19 @@ import log
 mysql_conf = cfg.get("mysql")
 # TODO: cursor connect
 
-db = pymysql.connect(host=mysql_conf["host"], user=mysql_conf["user"], passwd=mysql_conf["passwd"], db=mysql_conf["db"], charset=mysql_conf["charset"])
 
 def add_data(data_time, data_temperature, data_humidity):
-    cursor = db.cursor()
-    sql="INSERT INTO data(th_time, temperature, humidity) VALUES ('{0}', '{1}', '{2}')".format(data_time, data_temperature, data_humidity)
-    cursor.execute(sql)
-    db.commit()
-    log.logging.info("Added new temperature in DB! [time = {0}]".format(data_time))
+    try:
+        db = pymysql.connect(host=mysql_conf["host"], user=mysql_conf["user"], passwd=mysql_conf["passwd"], db=mysql_conf["db"], charset=mysql_conf["charset"])
+        cursor = db.cursor()
+        sql="INSERT INTO data(th_time, temperature, humidity) VALUES ('{0}', '{1}', '{2}')".format(data_time, data_temperature, data_humidity)
+        cursor.execute(sql)
+        db.commit()
+        log.logging.info("Added new temperature in DB!")
+        db.close()
+    except:
+        log.logging.error("Error in DB!")
+
+
+
+
